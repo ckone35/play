96944c3912cda7226569791fbe685261ae176bd0
François Richard <fr@frnet.fr>
2018-10-13 19:30:06 +0200

    Add external url testing

---
+++ src/main/java/fr/ckone35/play/web/GreetingController.java
@@ -0,0 +1,35 @@
+package fr.ckone35.play.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class GreetingController {
+
+
+    @Value("${external.url}")
+    private String externalUrl;
+
+    @GetMapping("/hello")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response
+                = restTemplate.getForEntity(externalUrl, String.class);
+
+        model.addAttribute("code", response.getStatusCode().value());
+
+        model.addAttribute("url", externalUrl);
+        System.out.println(response.getBody());
+        model.addAttribute("length", response.getBody().length());
+        model.addAttribute("name", name);
+
+        return "greeting";
+    }
+
+}
